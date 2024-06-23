72efcd1b9af64385272f3cc1602cfbaa4ad8a058
phanendra lanki <phanendralg12@gmail.com>
2024-06-23 18:17:29 +0530

    created header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,10 +4,13 @@ import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
+import Header from "./components/Header";
 const App = () => {
   return (
     <>
       <BrowserRouter>
+      {/* Header */}
+      <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/signin" element={<Signin />} />
