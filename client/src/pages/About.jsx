e2c9908d7491818f466d212fa8bb6705f713e0be
phanendra lanki <phanendralg12@gmail.com>
2024-06-25 09:44:59 +0530

    designed sample home and about pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,16 @@
 const About = () => {
   return (
-    <div>
-      About
+    <div className="px-4 py-12 max-w-2xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-700">About</h1>
+      <p className="mb-4 text-slate-700">
+      If your mind thinks about mobile/website development, then we have created a niche for ourselves. We started in 2012 with just 3 employees and now have expanded ourselves to 50+ which shows about growth and quality of work that we did over the years.
+      </p>
+      <p className="mb-4 text-slate-700">
+      If your mind thinks about mobile/website development, then we have created a niche for ourselves. We started in 2012 with just 3 employees and now have expanded ourselves to 50+ which shows about growth and quality of work that we did over the years.
+      </p>
+      <p className="mb-4 text-slate-700">
+      If your mind thinks about mobile/website development, then we have created a niche for ourselves. We started in 2012 with just 3 employees and now have expanded ourselves to 50+ which shows about growth and quality of work that we did over the years.
+      </p>
     </div>
   )
 }
